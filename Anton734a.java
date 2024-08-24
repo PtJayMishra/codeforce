4210b9e313c681d70f0585d9d45e1acf102ffbf2
PtJayMishra <142666593+PtJayMishra@users.noreply.github.com>
2024-08-25 03:16:47 +0530

    Update Anton734a.java

--- Anton734a.java
+++ Anton734a.java
@@ -1,23 +1,3 @@
-//import java.util.Scanner;
-//
-//public class Anton734a {
-//    public static void main(String[] args) {
-//        Scanner in = new Scanner(System.in);
-//        int n = in.nextInt();
-//        for (int i = 0; i < n; i++) {
-//            String s = in.next();
-//            for (int j = 0; j <s.length() ; j++) {
-//                if(s.charAt(j) == 'A') {
-//
-//                    System.out.println();
-//
-//                }
-//
-//            }
-//
-//        }
-//    }
-//}
 import java.util.Arrays;
 import java.util.Scanner;
 
